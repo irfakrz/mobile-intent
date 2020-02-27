b2f20b7dfa480f20e59715c154e804ac3c9a66ba
rezaLUL <rezaulull@gmail.com>
2020-02-27 21:43:57 +0700

    Bundle, implicit, explicit, parcelable

--- app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -1,15 +1,29 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
+    private EditText NameInput;
+    private TextView OutputText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+
+        NameInput = findViewById(R.id.input_name);
+        OutputText = findViewById(R.id.text_output);
     }
 
+    public void handleSumbmit(View view) {
+        String name = NameInput.getText().toString();
+        OutputText.setText(name);
+    }
 }
